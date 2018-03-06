56161f2263f35dd44ece80a35241c166fe5200c5
张阳洋 <zhangyangyang@zhangyaangdeAir.lan>
2018-03-06 20:18:40 +0800

    code style

--- src/config/default.js
+++ src/config/default.js
@@ -1,5 +1,5 @@
 const path = require('path')
 
 module.exports = {
-    screenshot: path.resolve(__dirname, '../../screenshot')
+  screenshot: path.resolve(__dirname, '../../screenshot')
 }
--- src/screenshot.js
+++ src/screenshot.js
@@ -1,15 +1,14 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
-const { screenshot } = require('./config/default');
+const { screenshot } = require('./config/default')
 
-(async () => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto('https://www.baidu.com')
-    await page.screenshot({
-        path:`${screenshot}/${Date.now()}.png`
-        
-    })
-    await browser.close()
-})();
+~(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://www.google.com')
+  await page.screenshot({
+    path: `${screenshot}/${Date.now()}.png`
 
+  })
+  await browser.close()
+})()
